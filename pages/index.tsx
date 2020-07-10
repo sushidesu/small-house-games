d9f89b660b87b7d5558809e0f11569bfda4259d0
sushidesu <hugemerge@gmail.com>
2020-07-10 07:27:11 +0000

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default () => {
+  return (
+    <div className="container">
+      <Head>
+        <title>small house games</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>small house games</h1>
+    </div>
+  )
+}
